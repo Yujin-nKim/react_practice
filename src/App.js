18927be234176a7912eea445242664b5f866a155
Yujin_nKim <yujin.kimd@gmail.com>
2022-07-07 13:25:11 +0900

    add event function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 
 function Header(props) {
   return <header>
-  <h1><a href="/">{props.title}</a></h1>
+  <h1><a href="/" onClick={(event)=>{
+    event.preventDefault();
+    props.onChangeMode();
+  }}>{props.title}</a></h1>
 </header>
 }
 
@@ -11,7 +14,10 @@ function Nav(props) {
   const lis = []
   for(let i=0; i<props.topics.length; i++) {
     let t = props.topics[i];
-    lis.push(<li><a href={'/read/'}>{t.title}</a></li>)
+    lis.push(<li key={t.id}><a id={t.id} href={'/read/'+t.id} onClick={event=>{
+      event.preventDefault();
+      props.onChangeMode(event.target.id);
+    }}>{t.title}</a></li>)
   }
   return <nav>
   <ol>
@@ -36,8 +42,12 @@ function App() {
   ]
   return (
     <div>
-        <Header title="REACT"></Header>
-        <Nav topics={topics}></Nav>
+        <Header title="REACT" onChangeMode={()=>{
+          alert('Header');
+        }}></Header>
+        <Nav topics={topics} onChangeMode={(id)=>{
+          alert(id);
+        }}></Nav>
         <Article title="Welcome" body ="Hello, WEB"></Article>
     </div>
   );
